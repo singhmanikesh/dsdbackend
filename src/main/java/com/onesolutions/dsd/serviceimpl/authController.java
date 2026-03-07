fcac09fb7cc1413c8f3e38ee190feccc5d868975
manikeshhhh <manikesh182003@gmail.com>
2026-03-07 13:41:08 +0530

    Project setup

---
+++ src/main/java/com/onesolutions/dsd/serviceimpl/authController.java
@@ -0,0 +1,17 @@
+package com.onesolutions.dsd.serviceimpl;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class authController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+
+
+}

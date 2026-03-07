6cc72232c754a7c3fa7194304c5b557308cc04d1
manikeshhhh <manikesh182003@gmail.com>
2026-03-07 12:39:29 +0530

    init

---
+++ src/main/java/com/onesolutions/dsd/DsdApplication.java
@@ -0,0 +1,13 @@
+package com.onesolutions.dsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onesolutions/dsd/DsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onesolutions.dsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

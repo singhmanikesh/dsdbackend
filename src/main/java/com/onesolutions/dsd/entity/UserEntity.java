5b9de5be31824def80e4eeb09c2ea4e1db7f80aa
manikeshhhh <manikesh182003@gmail.com>
2026-03-20 18:33:35 +0530

    fixed

--- src/main/java/com/onesolutions/dsd/entity/UserEntity.java
+++ src/main/java/com/onesolutions/dsd/entity/UserEntity.java
@@ -25,6 +25,7 @@ public class UserEntity {
     @Column(unique = true)
     private String email;
     private String password;
+    @Column(unique = true)
     private String gamerName;
     private String steamid;
     private String riotid;

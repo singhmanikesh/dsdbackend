60de88ce6c087903a91d5ba714be283005fa59b7
manikeshhhh <manikesh182003@gmail.com>
2026-03-21 14:16:16 +0530

    changed securiyt config

--- src/main/java/com/onesolutions/dsd/Config/SecurityConfig.java
+++ src/main/java/com/onesolutions/dsd/Config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:8080","http://localhost:5173")); // Use your frontend's actual origin
+        config.setAllowedOriginPatterns(List.of("http://localhost:8080","http://localhost:5173","http://localhost:5174")); // Use your frontend's actual origin
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
